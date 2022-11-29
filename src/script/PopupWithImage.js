7888c711804e3f85682f86b35026d130be2167ee
Dmitrii Spasskii <ddolan615@gmail.com>
2022-11-29 20:28:31 +0600

    dev: рефакторинг и webpack

--- src/script/PopupWithImage.js
+++ src/script/PopupWithImage.js
@@ -1,17 +1,16 @@
 import Popup from "./Popup.js";
 
-
-export default class PopupWithImage extends Popup{
-    constructor(popupSelector){
+export default class PopupWithImage extends Popup {
+    constructor(popupSelector) {
         super(popupSelector);
-        this._popupImgImg = document.querySelector('.popup__image');
-        this._popupImgText = document.querySelector('.popup__text');
-    }
-
-    open({name, link}){
+        this._popupImgImg = this._popup.querySelector('.popup__image');
+        this._popupImgText = this._popup.querySelector('.popup__text');
+    };
+    
+    open({ name, link }) {
         super.open();
         this._popupImgImg.src = link;
         this._popupImgImg.alt = name;
         this._popupImgText.textContent = name;
-      };
+    }; 
 }
--- src/script/index.js
+++ src/script/index.js
@@ -1,5 +1,5 @@
-
-
+import "../pages/index.css";
+import params from "./params.js";
 import { initialCards } from "./cards.js";
 import Card from "./Сard.js";
 import { FormValidator } from "./FormValidator.js";
@@ -7,8 +7,8 @@ import PopupWithForm from "./PopupWithForm.js";
 import PopupWithImage from "./PopupWithImage.js";
 import Section from "./Section.js";
 import UserInfo from "./UserInfo.js";
-import params from "./params.js"
 
+const cards = document.querySelector('.elements');
 const popupProfile = document.querySelector('#popup-profile');
 const profileEditButton = document.querySelector('.profile__edit');
 const author = popupProfile.querySelector('#popup-profile__form-box_type_author');
@@ -16,36 +16,33 @@ const job = popupProfile.querySelector('#popup-profile__form-box_type_job');
 const popupCard = document.querySelector('#popup-add');
 const profileAddCard = document.querySelector('.profile__add');
 
-
-
 const сardValidation = new FormValidator(params, popupCard);
 const profileValidation = new FormValidator(params, popupProfile);
 
-
-const createCard = (cardData) => {
-  const card = new Card (cardData, params.templateCardSelector, handleCardClick);
-  return card.createCard();
-};
-
 const section = new Section({
-    items: initialCards,
-    renderer: data => {
-        section.addItem(createCard(data));
-    }
+  items: initialCards,
+  renderer: data => {
+      section.addItem(createCard(data));
   },
-    '.elements'
-  );
-  section.renderCard();
+},
+  '.elements'
+);
+section.renderCard();
 
 const popupWithImage = new PopupWithImage('#popup-img');
 popupWithImage.setEventListeners();
 
 function handleCardClick(data) { 
-  popupWithImage.open(data);
+  popupWithImage.open(data)
+};
+
+function createCard(cardData) {
+  const card = new Card (cardData, params.templateCardSelector, handleCardClick);
+  return card.createCard();
 };
 
 function addCard(card) {
-    section.prepend(card);
+  cards.prepend(card);
 }
 
 const popupCardForm = new PopupWithForm(handleSubmitCard, '#popup-add');
@@ -54,7 +51,7 @@ function handleSubmitCard(evt, data) {
     evt.preventDefault();
     addCard(createCard(data));
     popupCardForm.close();
-  };
+};
 
 popupCardForm.setEventListeners();
 
@@ -75,7 +72,6 @@ function handleSubmitProfile(evt, data) {
   };
 
 popupProfileForm.setEventListeners();
-
 
 profileEditButton.addEventListener('click', () => {
     const profileInfo = userInfo.getUserInfo();
